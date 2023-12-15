95c457fa9cf092de026be9f98665a7a393e66e07
Francisco Carpio <carpiofj@gmail.com>
2023-12-15 12:15:54 +0100

    update hypr apps

--- arch_post.sh
+++ arch_post.sh
@@ -198,6 +198,8 @@ while [ "$1" != "" ]; do
         newgrp docker
         log "INFO" "setting alacritty default for nemo..."
         gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
+        log "INFO" "setting nemo default file manager"
+        xdg-mime default nemo.desktop inode/directory
         log "INFO" "adding virtualenv to pyenv..."
         git clone https://github.com/pyenv/pyenv-virtualenv.git "$(pyenv root)"/plugins/pyenv-virtualenv
         ;;
